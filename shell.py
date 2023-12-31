e47683a213905e0d1413afd5545f8edc13fb69cc
Admin <preciouspeter3173@gmail.com>
2023-12-31 23:39:47 +0100

    token updated

--- shell.py
+++ shell.py
@@ -1,8 +1,30 @@
-from rply import LexerGenerator
+import re
+import ply.lex as lex
 
 
-class shell():
- #  method is a special method that gets automatically called when you create a new object (instance) of the class
- # it's like a constructor in other programming language
- def __init__(self):
-    self.lexer = LexerGenerator()
+class shell:
+ 
+  tokens = (
+  'NUMBER',
+  'PLUS',
+  'MINUS',
+  'TIMES',
+  'DIVIDE',
+  'LPAREN',
+  'RPAREN',
+  'OR',
+  'AND',
+  'EQUALITY',
+  'INEQUALITY',
+  'GREATER',
+  'LESS'
+  )
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_OR = r'\|\|'
+t_AND = r'\&\&'
